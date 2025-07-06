2fd2207a80d097fa7ee6a451e18d363f6c1e13f2
Talus <xiaoxiaochenjian@gmail.com>
2025-07-06 19:37:23 +0800

    add onvif

--- src/onvif.cpp
+++ src/onvif.cpp
@@ -1,10 +1,4 @@
-/**
-* Copyright (c) 2025-2035 Shenzhen Lvlian Technology Co., Ltd
-*
-* @author       :   liangzhuohua
-* @created date :   2025/7/3
-* @version      :   1.0
-*/
+
 #include <cstdlib>
 #include "Network/TcpServer.h"
 #include "Network/Socket.h"
--- src/onvif.h
+++ src/onvif.h
@@ -1,10 +1,4 @@
-/**
-* Copyright (c) 2025-2035 Shenzhen Lvlian Technology Co., Ltd
-*
-* @author       :   liangzhuohua
-* @created date :   2025/7/3
-* @version      :   1.0
-*/
+
 #ifndef UG_SDK_ONVIF_H
 #define UG_SDK_ONVIF_H
 
